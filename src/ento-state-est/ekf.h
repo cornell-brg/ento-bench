bdcc39c14339a97433cc5c5b9828387ca52c1be8
Derin Ozturk <ddozturk7@gmail.com>
2025-02-27 12:53:16 -0500

    Added implementations of Towards Sensor Autonomy EKF and HIL for Embedded State Esitmation for Flying Microrobots EKF.

---
+++ src/ento-state-est/ekf.h
@@ -0,0 +1,190 @@
+#ifndef EKF_HH
+#define EKF_HH
+
+#include <Eigen/Dense>
+#include <concepts>
+
+template<typename T, typename Scalar, size_t StateDim, typename... ControlInputs>
+concept DynamicsModelConcept = requires(T t, Eigen::Matrix<Scalar, StateDim, 1>& x, ControlInputs... u, Eigen::Matrix<Scalar, StateDim, StateDim>& jacobian_matrix) {
+    { t(x, u...) } -> std::same_as<void>;
+    { t.jacobian(jacobian_matrix, x, u...) } -> std::same_as<void>;
+};
+
+template<typename T, typename Scalar, size_t StateDim, size_t MeasurementDim>
+concept MeasurementModelConcept = requires(T t, const Eigen::Matrix<Scalar, StateDim, 1>& x, Eigen::Matrix<Scalar, MeasurementDim, 1>& z, Eigen::Matrix<Scalar, MeasurementDim, StateDim>& jacobian_matrix) {
+    { t(x, z) } -> std::same_as<void>;
+    { t.jacobian(jacobian_matrix, x) } -> std::same_as<void>;
+};
+
+template<typename Scalar, size_t StateDim, size_t MeasurementDim, size_t ControlDim>
+class EKF
+{
+public:
+    using Scalar_ = Scalar;
+    static constexpr size_t StateDim_ = StateDim;
+    static constexpr size_t MeasurementDim_ = MeasurementDim;
+    static constexpr size_t ControlDim_ = ControlDim;
+
+    EKF(const Eigen::Matrix<Scalar, StateDim, StateDim>& Q, // Process Noise covariance matrix
+        const Eigen::Matrix<Scalar, MeasurementDim, MeasurementDim>& R) // Measurement Noise covariance matrix
+        : x_(Eigen::Matrix<Scalar, StateDim, 1>::Zero()),
+          P_(Eigen::Matrix<Scalar, StateDim, StateDim>::Identity()), // P_: State Covariance Matrix
+          predicted_measurement_(Eigen::Matrix<Scalar, MeasurementDim, 1>::Zero()), // Measurement predicted from dynamics
+          measurement_residual_(Eigen::Matrix<Scalar, MeasurementDim, 1>::Zero()), // Residual (measurement - predicted measurement)
+          Q_(Q), // Process Noise Covariance Matrix
+          R_(R) // Measurement Covariance Matrix
+    {}
+
+    template <typename DynamicsModel, typename... ControlInputs>
+    requires DynamicsModelConcept<DynamicsModel, Scalar, StateDim, ControlInputs...>
+    void predict(const DynamicsModel& dynamics, ControlInputs... u)
+    {
+      // Predict state using dynamics model
+      dynamics(x_, u...);
+
+      Eigen::Matrix<Scalar, StateDim, StateDim> F;
+      dynamics.jacobian(F, x_, u...);
+
+      // Update the covariance.
+      P_ = F * P_ * F.transpose() + Q_;
+    }
+
+    template <typename MeasurementModel>
+    requires MeasurementModelConcept<MeasurementModel, Scalar, StateDim, MeasurementDim>
+    void update(const MeasurementModel& meas_model, const Eigen::Matrix<Scalar, MeasurementDim, 1>& z)
+    {
+        // Predict measurement
+        meas_model(x_, predicted_measurement_);
+
+        // Compute the measurement Jacobian evaluated at x.
+        Eigen::Matrix<Scalar, MeasurementDim, StateDim> H;
+        meas_model.jacobian(H, x_);
+
+        // Compute innovation covariance S and Kalman gain K.
+        Eigen::Matrix<Scalar, MeasurementDim, MeasurementDim> S = H * P_ * H.transpose() + R_;
+        Eigen::Matrix<Scalar, StateDim, MeasurementDim> K = P_ * H.transpose() * S.inverse();
+
+        // Compute the measurement residual (measured - predicted)
+        measurement_residual_ = z - predicted_measurement_;
+
+        // Update the state estimate using the residual and Kalman Gain.
+        x_ += K * measurement_residual_;
+        Eigen::Matrix<Scalar, StateDim, StateDim> I = Eigen::Matrix<Scalar, StateDim, StateDim>::Identity();
+
+        // Update the covariance.
+        P_ = (I - K * H) * P_;
+    }
+
+    template <typename MeasurementModel>
+    requires MeasurementModelConcept<MeasurementModel, Scalar, StateDim, MeasurementDim>
+    void update_sequential(const MeasurementModel& meas_model,
+                           const Eigen::Matrix<Scalar, MeasurementDim, 1>& z,
+                           [[maybe_unused]] const Eigen::Matrix<Scalar, ControlDim, 1>& u,
+                           std::array<bool, MeasurementDim>& avail_sensors)
+    {
+      static constexpr Scalar xi = 1e6;
+      Eigen::Matrix<Scalar, MeasurementDim, MeasurementDim> I_meas =
+          Eigen::Matrix<Scalar, MeasurementDim, MeasurementDim>::Identity();
+      for (size_t j = 0; j < MeasurementDim; ++j)
+      {
+        // Step (4)
+        Eigen::Matrix<Scalar, MeasurementDim, MeasurementDim> Rtilde = xi * I_meas;
+
+        // Step (5)
+        if (avail_sensors[j])
+        {
+          // Step (5)
+          Rtilde(j, j) = R_(j, j);
+
+          // Step (6)
+          Eigen::Matrix<Scalar, MeasurementDim, 1> pred_meas;
+          meas_model(x_, u, pred_meas);
+          Eigen::Matrix<Scalar, MeasurementDim, StateDim> Hk;
+          meas_model.jacobian(Hk, x_);
+
+          // Step (7)
+          Eigen::Matrix<Scalar, MeasurementDim, MeasurementDim> S = Hk * P_ * Hk.transpose() + Rtilde;
+          Eigen::Matrix<Scalar, StateDim, MeasurementDim> K = P_ * Hk.transpose() * S.inverse();
+
+          // Step (8)
+          //Eigen::Matrix<Scalar, MeasurementDim, 1> measurement_residual;
+          measurement_residual_ = z - pred_meas;
+          x_ += K * measurement_residual_;
+
+          // Step (9)
+          Eigen::Matrix<Scalar, StateDim, StateDim> I = Eigen::Matrix<Scalar, StateDim, StateDim>::Identity();
+          P_ = (I - K * Hk) * P_;
+        }
+      }
+    }
+    
+    template <typename MeasurementModel>
+    requires MeasurementModelConcept<MeasurementModel, Scalar, StateDim, MeasurementDim>
+    void update_truncated(const MeasurementModel& meas_model,
+                          const Eigen::Matrix<Scalar, MeasurementDim, 1>& z,
+                          [[maybe_unused]] const Eigen::Matrix<Scalar, ControlDim, 1>& u,
+                          std::array<bool, MeasurementDim> & avail_sensors)
+    {
+      Eigen::Matrix<Scalar, Eigen::Dynamic, Eigen::Dynamic, 0, StateDim, StateDim> H_tilde;
+      Eigen::Matrix<Scalar, Eigen::Dynamic, Eigen::Dynamic, 0, MeasurementDim, MeasurementDim> R_tilde;
+
+      Eigen::Matrix<Scalar, Eigen::Dynamic, 1, 0, MeasurementDim, 1> y_tilde;
+      Eigen::Matrix<Scalar, Eigen::Dynamic, 1, 0, MeasurementDim, 1> z_tilde;
+
+      Eigen::Matrix<Scalar, MeasurementDim, 1> pred_meas;
+      meas_model(x_, u, pred_meas);
+
+      Eigen::Matrix<Scalar, MeasurementDim, StateDim> Hk;
+      meas_model.jacobian(Hk, x_);
+
+      size_t m;
+      for (size_t j = 0; j < MeasurementDim; ++j)
+      {
+        if (avail_sensors[j])
+        {
+          y_tilde(m)     = z(j);
+          z_tilde(m)     = pred_meas(j);
+          R_tilde(m, m)  = R_(j, j);
+          H_tilde.row(m) = Hk.row(j);
+          ++m;
+        }
+      }
+
+      if (m == 0) return;
+
+      Eigen::Matrix<Scalar, Eigen::Dynamic, Eigen::Dynamic, 0, MeasurementDim, MeasurementDim> S;
+      S = H_tilde.topRows(m) * P_ * H_tilde.topRows(m).transpose() + R_tilde.topLeftCorner(m, m);
+      Eigen::Matrix<Scalar, StateDim, Eigen::Dynamic, 0, StateDim, MeasurementDim> K;
+      K = P_ * H_tilde.topRows(m).transpose() * S.inverse();
+
+      Eigen::Matrix<Scalar, Eigen::Dynamic, 1, 0, MeasurementDim, 1> measurement_residual =
+        y_tilde.topRows(m) - z_tilde.topRows(m);
+
+      x_ += K * measurement_residual;
+
+      Eigen::Matrix<Scalar, StateDim, StateDim> I = Eigen::Matrix<Scalar, StateDim, StateDim>::Identity();
+      P_ = ( I - K * H_tilde.topRows(m) ) * P_;
+    }
+
+    Eigen::Matrix<Scalar, StateDim, 1> getState() const
+    {
+        return x_;
+    }
+
+    Eigen::Matrix<Scalar, StateDim, StateDim> getCovariance() const
+    {
+        return P_;
+    }
+
+private:
+    Eigen::Matrix<Scalar, StateDim, 1> x_; // State vector
+    Eigen::Matrix<Scalar, StateDim, StateDim> P_; // Covariance matrix
+    Eigen::Matrix<Scalar, MeasurementDim, 1> predicted_measurement_;
+    Eigen::Matrix<Scalar, MeasurementDim, 1> measurement_residual_;
+    Eigen::Matrix<Scalar, StateDim, StateDim> Q_; // Process noise covariance matrix
+    Eigen::Matrix<Scalar, MeasurementDim, MeasurementDim> R_; // Measurement noise covariance matrix
+    Eigen::Matrix<Scalar, StateDim, StateDim> F_; // Fixed state transition Jacobian
+    Eigen::Matrix<Scalar, MeasurementDim, StateDim> H_; // Fixed measurement Jacobian
+};
+
+#endif // EKF_HH
---
+++ src/ento-state-est/robofly_ekf_v1.h
@@ -0,0 +1,70 @@
+#include <Eigen/Dense>
+#include <cmath>
+#include <iostream>
+
+template <typename Scalar>
+struct RoboFlyV1DynamicsModel
+{
+  void operator()(Eigen::Matrix<Scalar, 4, 1>& x,
+                  Eigen::Matrix<Scalar, 1, 1>& u,
+                  Scalar dt)
+  {
+    Scalar theta = x(0);
+    Scalar vx    = x(1);
+    Scalar z     = x(2);
+    Scalar vz    = x(3);
+
+    Scalar omega = u(0);
+
+    Eigen::Matrix<Scalar, 4, 1> f_c;
+    f_c(0) = omega;
+    f_c(1) = Scalar(0);
+    f_c(2) = vz;
+    f_c(3) = Scalar(0);
+
+    x += dt * f_c;
+  }
+  constexpr void jacobian(Eigen::Matrix<Scalar, 4, 4>& A,
+                          [[maybe_unused]] Eigen::Matrix<Scalar, 4, 1>& x,
+                          [[maybe_unused]] Eigen::Matrix<Scalar, 1, 1>& u)
+  {
+    A.setZero();
+    A(2, 3) = 1;
+  }
+};
+
+template <typename Scalar>
+struct RoboFlyV1MeasurementModel
+{
+  static constexpr Scalar g_ = 9.81;
+
+  inline void operator()(const Eigen::Matrix<Scalar, 4, 1>& x,
+                         const Eigen::Matrix<Scalar, 1, 1>& u,
+                               Eigen::Matrix<Scalar, 4, 1>& pred) const
+  {
+    Scalar theta = x(0);
+    Scalar vx    = x(1);
+    Scalar z     = x(2);
+    Scalar vz    = x(3);
+
+    Scalar omega = u(0);
+
+    // Pre-compute cos/sin theta
+    Scalar cos_theta = std::cos(theta);
+    Scalar sin_theta = std::sin(theta);
+
+    pred(0) = z / cos(theta);
+    pred(1) = (cos_theta / z) * (vx * cos_theta + vz * sin_theta) - omega;
+    pred(2) = -g_ * sin_theta;
+    pred(3) = -g_ * cos_theta;
+  }
+
+  void jacobian(Eigen::Matrix<Scalar, 4, 4>& H,
+                Eigen::Matrix<Scalar, 4, 1>& x)
+  {
+    H.setZero();
+    H(0, 2) = 1;
+    H(1, 1) = Scalar(1) / x[2];
+    H(2, 0) = g_;
+  }
+};
