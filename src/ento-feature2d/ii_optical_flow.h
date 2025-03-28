15cb6ff26009c366e0c1e0dda1b5fc5006febbb9
Angela Cui <ayc62@cornell.edu>
2025-03-28 18:22:38 -0500

    initial image interpolation optical flow

---
+++ src/ento-feature2d/ii_optical_flow.h
@@ -0,0 +1,111 @@
+#ifndef II_OPTICAL_FLOW_H
+#define II_OPTICAL_FLOW_H
+#include <iostream>
+#include <math.h>
+#include <Eigen/Dense>
+#include <math/FixedPoint.hh>
+#include <image_io/Image.h>
+#include <ento-feature2d/feat2d_util.h>
+
+
+
+using namespace EntoFeature2D;
+
+
+// x_shamt is the reference amount to shift prevImg along the +-x direction
+// y_shamt is the reference amount to shift prevImg along the +-y direction
+// Let f be the nextImg and f0 be the prevImg
+// Define f1 = f0(x + x_shamt) and f2 = f0(x - x_shamt)
+// Define f3 = f0(y + y_shamt) and f4 = f0(y - y_shamt)
+// We define the velocity vector as (u,v) in pixel units
+template <int IMG_WIDTH, int IMG_HEIGHT, int WIN_DIM, typename CoordT, typename PixelT>
+void calcOpticalFlowII(const Image<IMG_HEIGHT, IMG_WIDTH, PixelT>& prevImg, 
+                                const Image<IMG_HEIGHT, IMG_WIDTH, PixelT>& nextImg,
+                                Keypoint<CoordT>* prevPts, Keypoint<CoordT>* nextPts,
+                               bool* status, int num_good_points,
+                               int x_shamt, 
+                               int y_shamt,
+                               int DET_EPSILON)
+{
+    int halfWin = (int) WIN_DIM / 2;
+
+    // Compute (f2 - f1), (f4-f3), 
+
+    // iterate over num points
+    for (int i = 0; i < num_good_points; i++ ) {
+
+        Keypoint<CoordT> pt = prevPts[i];
+
+        // Indices for top left corner of window
+        int32_t x_i = static_cast<int32_t>(pt.x)-halfWin;
+        int32_t y_i = static_cast<int32_t>(pt.y)-halfWin;
+
+        // Ix = f2 - f1; Iy = f4-f3; It = f-f0
+        float IxIx = 0;
+        float IyIy = 0;
+        float IxIy = 0;
+        float ItIx = 0;
+        float ItIy = 0;
+
+        // y coord
+        for (int j = 0; j < WIN_DIM; j++) {
+            // x coord
+            for (int k = 0; k < WIN_DIM; k++) {
+
+                int x_index = std::min(std::max(x_i + j, 0), prevImg.cols - 1);
+                int x_l_index = std::min(std::max(x_index + j - x_shamt, 0), prevImg.cols - 1);
+                int x_r_index = std::min(std::max(x_index + j + x_shamt, 0), prevImg.cols - 1);
+
+                int y_index = std::min(std::max(y_i + k, 0), prevImg.rows - 1);
+                int y_b_index = std::min(std::max(y_index + k - y_shamt, 0), prevImg.rows - 1);
+                int y_t_index = std::min(std::max(y_index + k + y_shamt, 0), prevImg.rows - 1);
+
+
+                int Ix = prevImg.get_pixel(y_index, x_r_index) - prevImg.get_pixel(y_index, x_l_index);
+                int Iy = prevImg.get_pixel(y_t_index, x_index) - prevImg.get_pixel(y_b_index, x_index);
+                int It = nextImg.get_pixel(y_index, x_index) - prevImg.get_pixel(y_index, x_index);
+                
+                IxIx += Ix * Ix;
+                IyIy += Iy * Iy;
+                IxIy += Ix * Iy;
+                ItIx += It * Ix;
+                ItIy += It * Iy;
+            }
+        }
+
+        IxIx = IxIx / (WIN_DIM * WIN_DIM);
+        IyIy = IyIy / (WIN_DIM * WIN_DIM);
+        IxIy = IxIy / (WIN_DIM * WIN_DIM);
+        ItIx = ItIx / (WIN_DIM * WIN_DIM);
+        ItIy = ItIy / (WIN_DIM * WIN_DIM);
+
+        // We get the system of equations:
+        // | Ix^2   IxIy | | u/x_shamt |   | 2ItIx |
+        // |             | |           | = |       |
+        // | IxIy   Iy^2 | | v/y_shamt | = | 2ItIy |
+
+        // The determinant of the left matrix is (IxIx * IyIy - IxIy * IxIy)
+        // The inverse is 
+        // | Iy^2   -IxIy |
+        // |              | * 1/det
+        // | -IxIy   Ix^2 |
+
+
+        float det = IxIx * IyIy - IxIy * IxIy;
+        if (static_cast<int32_t>(det) < DET_EPSILON && static_cast<int32_t>(det) > (-DET_EPSILON)) {
+            status[i] = 0;
+            nextPts[i] = pt;
+            continue;
+        }
+
+        CoordT u = (((CoordT) IyIy * (CoordT) (2*ItIx)) - ((CoordT) IxIy * (CoordT) (2*ItIy))) / (CoordT) det;
+        CoordT v = (((CoordT) IxIx * (CoordT) (2*ItIy)) - ((CoordT) IxIy * (CoordT) (2*ItIx)))  / (CoordT) det;
+        
+        Keypoint<CoordT> nextPt(pt.x + u * (CoordT)x_shamt, pt.y + v * (CoordT)y_shamt);
+        nextPts[i] = nextPt;
+        status[i] = 1;
+
+    }
+}
+
+#endif
---
+++ src/ento-feature2d/test/test_ii_optical_flow.cc
@@ -0,0 +1,88 @@
+#include <stdlib.h>
+#include <Eigen/Dense>
+
+#include <ento-util/debug.h>
+#include <math/FixedPoint.hh>
+#include <ento-feature2d/feat2d_util.h>
+
+#include <ento-feature2d/ii_optical_flow.h>
+#include <ento-util/unittest.h>
+#include <iostream>
+
+using namespace std;
+using namespace Eigen;
+using namespace EntoUtil;
+using namespace EntoFeature2D;
+
+
+void test_ii_optical_flow_basic() {
+    // initialize point arrays
+    Keypoint<float> *prevPts = new Keypoint<float>[2];
+    Keypoint<float> *nextPts = new Keypoint<float>[2];
+    bool *status_simple = new bool[2];
+
+    // initialize point info
+    // first point is valid feature
+    // second point is invalid feature
+    float x_1(197.0f);
+    float y_1(106.0f);
+    float x_2(50.0f);
+    float y_2(50.0f);
+    prevPts[0] = Keypoint<float>(x_1, y_1);
+    nextPts[0] = Keypoint<float>(x_1, y_1);
+    prevPts[1] = Keypoint<float>(x_2, y_2);
+    nextPts[1] = Keypoint<float>(x_2, y_2);
+
+    string top_str = "/Users/acui21/Documents/brg/pgm_images/image_2.pgm";
+    const char* prev_image_path = top_str.c_str();
+    constexpr int DIM = 320;
+    Image<DIM, DIM, uint8_t> prevImg;
+    ENTO_TEST_CHECK_INT_EQ(prevImg.image_from_pgm(prev_image_path), 1);
+
+    string next_str = "/Users/acui21/Documents/brg/ento-bench/src/ento-feature2d/test/image_2_1y.pgm";
+    const char* next_image_path = next_str.c_str();
+    Image<DIM, DIM, uint8_t> nextImg;
+    ENTO_TEST_CHECK_INT_EQ(nextImg.image_from_pgm(next_image_path), 1);
+
+    constexpr int WIN_DIM = 25; 
+    // int DET_EPSILON = (int)(1<<20);
+    int DET_EPSILON = 0;
+
+    calcOpticalFlowII<DIM, DIM, WIN_DIM, float, uint8_t>(prevImg, nextImg,
+        prevPts, nextPts, status_simple, 2, 7, 7, DET_EPSILON);
+
+    for (int i = 0; i < 2; i++) {
+        cout << nextPts[i].x << " " << nextPts[i].y << endl;
+        cout << status_simple[i] << endl;
+    }
+    // float x_1(197.0f);
+    // float y_1(106.0f);
+    ENTO_TEST_CHECK_FLOAT_EQ(nextPts[0].x, 197.0f);
+    ENTO_TEST_CHECK_FLOAT_EQ(nextPts[0].y, 107.0f);
+
+    delete [] prevPts;
+    delete [] nextPts;
+    delete [] status_simple;
+
+
+}
+
+int main ( int argc, char ** argv)
+{
+  using namespace EntoUtil;
+  int __n;
+  if (argc > 1)
+  {
+    __n = atoi(argv[1]);
+  }
+  else
+  {
+    // For the case we are running on the MCU and we can't pass in args
+    // the same way args are passed for a native build.
+    __ento_replace_file_suffix(__FILE__, "test_lk_optical_flow_cmdline.txt");
+    __n = __ento_get_test_num_from_file(__ento_cmdline_args_path_buffer);
+  }
+
+  if (__ento_test_num(__n, 1)) test_ii_optical_flow_basic();
+
+}
