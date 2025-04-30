a35801ea289e87f5dd6766276492ec1cfe5be5f6
Angela Cui <ayc62@cornell.edu>
2025-04-30 09:10:58 -0400

    more ii optical flow tests

--- src/ento-feature2d/test/test_ii_optical_flow.cc
+++ src/ento-feature2d/test/test_ii_optical_flow.cc
@@ -14,8 +14,50 @@ using namespace Eigen;
 using namespace EntoUtil;
 using namespace EntoFeature2D;
 
+void test_ii_optical_flow_basic_y() {
+    // initialize point arrays
+    Keypoint<float> *prevPts = new Keypoint<float>[1];
+    Keypoint<float> *nextPts = new Keypoint<float>[1];
+    bool *status_simple = new bool[1];
 
-void test_ii_optical_flow_basic() {
+    // initialize point info
+    // first point is valid feature
+    // second point is invalid feature
+    float x_1(197.0f);
+    float y_1(106.0f);
+    prevPts[0] = Keypoint<float>(x_1, y_1);
+    nextPts[0] = Keypoint<float>(x_1, y_1);
+
+    string top_str = "/Users/acui21/Documents/brg/pgm_images/image_2.pgm";
+    const char* prev_image_path = top_str.c_str();
+    constexpr int DIM = 320;
+    Image<DIM, DIM, uint8_t> prevImg;
+    ENTO_TEST_CHECK_INT_EQ(prevImg.image_from_pgm(prev_image_path), 1);
+
+    string next_str = "/Users/acui21/Documents/brg/ento-bench/src/ento-feature2d/test/image_2_shifted_y.pgm";
+    const char* next_image_path = next_str.c_str();
+    Image<DIM, DIM, uint8_t> nextImg;
+    ENTO_TEST_CHECK_INT_EQ(nextImg.image_from_pgm(next_image_path), 1);
+
+    constexpr int WIN_DIM = 25; 
+    // int DET_EPSILON = (int)(1<<20);
+    int DET_EPSILON = 0;
+
+    int ref_motion = 7;
+
+    calcOpticalFlowII<DIM, DIM, WIN_DIM, float, uint8_t>(prevImg, nextImg,
+        prevPts, nextPts, status_simple, 2, ref_motion, ref_motion, DET_EPSILON);
+
+    ENTO_TEST_CHECK_FLOAT_EQ(nextPts[0].x, 197.0f);
+    ENTO_TEST_CHECK_FLOAT_EQ(nextPts[0].y, 107.0f);
+
+    delete [] prevPts;
+    delete [] nextPts;
+    delete [] status_simple;
+
+}
+
+void test_ii_optical_flow_basic_x() {
     // initialize point arrays
     Keypoint<float> *prevPts = new Keypoint<float>[2];
     Keypoint<float> *nextPts = new Keypoint<float>[2];
@@ -26,8 +68,8 @@ void test_ii_optical_flow_basic() {
     // second point is invalid feature
     float x_1(197.0f);
     float y_1(106.0f);
-    float x_2(50.0f);
-    float y_2(50.0f);
+    float x_2(220.0f);
+    float y_2(130.0f);
     prevPts[0] = Keypoint<float>(x_1, y_1);
     nextPts[0] = Keypoint<float>(x_1, y_1);
     prevPts[1] = Keypoint<float>(x_2, y_2);
@@ -39,7 +81,7 @@ void test_ii_optical_flow_basic() {
     Image<DIM, DIM, uint8_t> prevImg;
     ENTO_TEST_CHECK_INT_EQ(prevImg.image_from_pgm(prev_image_path), 1);
 
-    string next_str = "/Users/acui21/Documents/brg/ento-bench/src/ento-feature2d/test/image_2_1y.pgm";
+    string next_str = "/Users/acui21/Documents/brg/ento-bench/src/ento-feature2d/test/image_2_shifted_x.pgm";
     const char* next_image_path = next_str.c_str();
     Image<DIM, DIM, uint8_t> nextImg;
     ENTO_TEST_CHECK_INT_EQ(nextImg.image_from_pgm(next_image_path), 1);
@@ -51,20 +93,62 @@ void test_ii_optical_flow_basic() {
     calcOpticalFlowII<DIM, DIM, WIN_DIM, float, uint8_t>(prevImg, nextImg,
         prevPts, nextPts, status_simple, 2, 7, 7, DET_EPSILON);
 
-    for (int i = 0; i < 2; i++) {
-        cout << nextPts[i].x << " " << nextPts[i].y << endl;
-        cout << status_simple[i] << endl;
-    }
-    // float x_1(197.0f);
-    // float y_1(106.0f);
-    ENTO_TEST_CHECK_FLOAT_EQ(nextPts[0].x, 197.0f);
-    ENTO_TEST_CHECK_FLOAT_EQ(nextPts[0].y, 107.0f);
+    ENTO_TEST_CHECK_FLOAT_EQ(nextPts[0].x, 198.0f);
+    ENTO_TEST_CHECK_FLOAT_EQ(nextPts[0].y, 106.0f);
+    ENTO_TEST_CHECK_FLOAT_EQ(nextPts[1].x, 221.0f);
+    ENTO_TEST_CHECK_FLOAT_EQ(nextPts[1].y, 130.0f);
+
 
     delete [] prevPts;
     delete [] nextPts;
     delete [] status_simple;
+}
+
+void test_ii_optical_flow_basic_xy() {
+    // initialize point arrays
+    Keypoint<float> *prevPts = new Keypoint<float>[2];
+    Keypoint<float> *nextPts = new Keypoint<float>[2];
+    bool *status_simple = new bool[2];
+
+    // initialize point info
+    // first point is valid feature
+    // second point is invalid feature
+    float x_1(197.0f);
+    float y_1(106.0f);
+    float x_2(197.0f);
+    float y_2(106.0f);
+    prevPts[0] = Keypoint<float>(x_1, y_1);
+    nextPts[0] = Keypoint<float>(x_1, y_1);
+    prevPts[1] = Keypoint<float>(x_2, y_2);
+    nextPts[1] = Keypoint<float>(x_2, y_2);
+
+    string top_str = "/Users/acui21/Documents/brg/pgm_images/image_2.pgm";
+    const char* prev_image_path = top_str.c_str();
+    constexpr int DIM = 320;
+    Image<DIM, DIM, uint8_t> prevImg;
+    ENTO_TEST_CHECK_INT_EQ(prevImg.image_from_pgm(prev_image_path), 1);
+
+    string next_str = "/Users/acui21/Documents/brg/ento-bench/src/ento-feature2d/test/image_2_shifted_xy.pgm";
+    const char* next_image_path = next_str.c_str();
+    Image<DIM, DIM, uint8_t> nextImg;
+    ENTO_TEST_CHECK_INT_EQ(nextImg.image_from_pgm(next_image_path), 1);
+
+    constexpr int WIN_DIM = 25; 
+    // int DET_EPSILON = (int)(1<<20);
+    int DET_EPSILON = 0;
+
+    calcOpticalFlowII<DIM, DIM, WIN_DIM, float, uint8_t>(prevImg, nextImg,
+        prevPts, nextPts, status_simple, 2, 7, 7, DET_EPSILON);
+
+    ENTO_TEST_CHECK_FLOAT_EQ(nextPts[0].x, 198.0f);
+    ENTO_TEST_CHECK_FLOAT_EQ(nextPts[0].y, 107.0f);
+    ENTO_TEST_CHECK_FLOAT_EQ(nextPts[1].x, 198.0f);
+    ENTO_TEST_CHECK_FLOAT_EQ(nextPts[1].y, 107.0f);
 
 
+    delete [] prevPts;
+    delete [] nextPts;
+    delete [] status_simple;
 }
 
 int main ( int argc, char ** argv)
@@ -83,6 +167,8 @@ int main ( int argc, char ** argv)
     __n = __ento_get_test_num_from_file(__ento_cmdline_args_path_buffer);
   }
 
-  if (__ento_test_num(__n, 1)) test_ii_optical_flow_basic();
+  if (__ento_test_num(__n, 1)) test_ii_optical_flow_basic_y();
+  if (__ento_test_num(__n, 2)) test_ii_optical_flow_basic_x();
+  if (__ento_test_num(__n, 3)) test_ii_optical_flow_basic_xy();
 
 }
