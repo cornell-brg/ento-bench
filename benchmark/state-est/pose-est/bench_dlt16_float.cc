f98f92002828b7debc535d3af0b96148645c0037
Derin Ozturk <ddozturk7@gmail.com>
2025-06-14 15:09:26 -0400

    Fix dlt16 float.

--- benchmark/state-est/pose-est/bench_dlt16_float.cc
+++ benchmark/state-est/pose-est/bench_dlt16_float.cc
@@ -31,40 +31,10 @@ int main()
   __enable_irq();
 
   // Generic cache setup via config macro
-#include <ento-bench/harness.h>
-#include <ento-util/file_path_util.h>
-#include <ento-util/debug.h>
-#include <ento-util/unittest.h>
-#include <ento-bench/bench_config.h>
-
-#include <ento-mcu/cache_util.h>
-#include <ento-mcu/flash_util.h>
-#include <ento-mcu/clk_util.h>
-#include <ento-pose/data_gen.h>
-#include <ento-pose/prob_gen.h>
-#include <ento-pose/pose_util.h>
-#include <ento-pose/abs-pose/dlt.h>
-
-extern "C" void initialise_monitor_handles(void);
-
-using namespace EntoBench;
-using namespace EntoUtil;
-
-int main()
-{
-  using Scalar  = float;
-  using Solver  = EntoPose::SolverDLT<Scalar>;
-  using Problem = EntoPose::AbsolutePoseProblem<Scalar, Solver, 16>;
-  constexpr Scalar tol = 1e-4;
-  initialise_monitor_handles();
-
-  // Configure clock
-  sys_clk_cfg();
-  SysTick_Setup();
-  __enable_irq();
-
-  // Generic cache setup via config macro
   ENTO_BENCH_SETUP();
+
+  // Print benchmark configuration
+  ENTO_BENCH_PRINT_CONFIG();
 
   const char* base_path = DATASET_PATH;
   const char* rel_path = "abs-pose/dlt16_float_noise0.01.csv";
